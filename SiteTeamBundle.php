755e29d602eb309728c5a52acc3e9406df3bf685
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2020-09-16 12:49:04 +0200

    Create SiteTeamBundle.php

---
+++ SiteTeamBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of [package name].
+ *
+ * (c) John Doe
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace LocalbrandingDe\SiteTeamBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SiteTeamBundle extends Bundle
+{
+
+}
