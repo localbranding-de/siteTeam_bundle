380cd7bbf9cf8734ddb00bc8b3fdcccbcc09df98
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2020-11-05 13:57:31 +0100

    Update SiteTeamExtension.php

--- src/DependencyInjection/SiteTeamExtension.php
+++ src/DependencyInjection/SiteTeamExtension.php
@@ -15,7 +15,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class ExtendedProductDetailExtension extends Extension
+class SiteTeamExtension extends Extension
 {
     /**
      * {@inheritdoc}
